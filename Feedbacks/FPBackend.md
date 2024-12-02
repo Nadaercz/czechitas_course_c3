1c9f63689fb9d728b139969d3016250532dcb054
ondrej.kasparek <ondrej.kasparek@thermofisher.com>
2024-12-02 18:12:09 +0100

    FT Backend Feedback

---
+++ Feedbacks/FPBackend.md
@@ -0,0 +1,4 @@
+# Feedback Final Project Backend
+
+Great work, I did not encounter any problem. Only please remove the unnecessary code :wink:
+Otherwise I applaud that everything works and unit test are green like a grass during beautiful spring.
--- ToDoList/src/ToDoList.Domain/DTOs/ToDoItemGetResponseDto.cs
+++ ToDoList/src/ToDoList.Domain/DTOs/ToDoItemGetResponseDto.cs
@@ -6,5 +6,5 @@ using ToDoList.Domain.Models;
 public record class ToDoItemGetResponseDto(int Id, string Name, string? Description, bool IsCompleted, string? Category) //let client know the Id
 {
     public static ToDoItemGetResponseDto FromDomain(ToDoItem item) => new(item.ToDoItemId, item.Name, item.Description, item.IsCompleted, item.Category);
-    public int Count() => throw new NotImplementedException();
+    public int Count() => throw new NotImplementedException(); //if we dont use this, then we can remove it :)
 }
